04e97e2db375363cac86a90a0dad388c5eae7d01
shmutalov <shmutalov@gmail.com>
2015-06-03 15:18:08 +0500

    Some changes

--- gen/com/epikur/ismlar/R.java
+++ gen/com/epikur/ismlar/R.java
@@ -17,25 +17,37 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int gender_female=0x7f020000;
+        public static final int gender_male=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int list_selector=0x7f020003;
+        public static final int name_list_item_bg=0x7f020004;
+        public static final int name_list_item_bg_hover=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090005;
+        public static final int gender_image=0x7f090003;
+        public static final int name=0x7f090004;
+        public static final int names_list=0x7f090001;
+        public static final int search_names=0x7f090000;
+        public static final int thumbnail=0x7f090002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int name_list_item=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
+        public static final int search_names_hint=0x7f060003;
     }
     public static final class style {
         /** 
@@ -59,10 +71,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+    }
+    public static final class xml {
+        public static final int searchable=0x7f040000;
     }
 }
